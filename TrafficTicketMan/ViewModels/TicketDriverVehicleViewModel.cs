0fb1d390b8a5b856e08d648003ffe054b9b4dfe7
liilonga <kuloh2@gmail.com>
2013-12-01 22:43:49 +0200

    viewmodel changes

---
+++ TrafficTicketMan/ViewModels/TicketDriverVehicleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrafficTicketMan.Models;
+
+namespace TrafficTicketMan.ViewModels
+{
+    public class TicketDriverVehicleViewModel
+    {
+        public Ticket Ticket { get; set; }
+        public OffenderDriver OffenderDriver { get; set; }
+        public OffenderVehicle OffenderVehicle { get; set; }
+    }
+}
